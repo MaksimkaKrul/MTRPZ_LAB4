33608bf9c96016a4252d0a37c55f13041277cc30
MaksimkaKrul <krulevckiym@gmail.com>
2025-05-25 02:05:28 +0300

    Reverting last commit for constistent PR structure

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,1 +1,16 @@
-# Класи Task
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Task:
+    id: int
+    title: str
+    description: str = ""
+    status: str = "todo"
+    due_date: datetime = None
+
+class TaskNotFoundError(Exception):
+    pass
+
+class InvalidStatusError(Exception):
+    pass
